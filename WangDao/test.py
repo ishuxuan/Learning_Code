e6a86948a75fa5fa1e1a7e6f7fb33995dfc5a1e5
Ishuxuan <1733183066@qq.com>
2025-01-07 11:21:22 +0800

    我要用git

--- WangDao/test.py
+++ WangDao/test.py
@@ -1,13 +1,6 @@
 # 作者:胡轩
 # 2024年12月25日23时27分25秒
 # 1733183066@qq.com
-a=[1,2,3]
-b=[2,3]
-c=[a,b,6]
-d=c.copy()
-a[0]=3
-c[2]=5
-print(c)
-print(id(c))
-print(d)
-print(id(d))
+import numpy as np
+a=np.array(i for i in range(6))
+print(a)
